236400db1809455158c027d54efb628d20a146f5
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-10-27 11:47:17 -0700

    just worked on two lessons today

---
+++ Javascript/fortune.js
@@ -0,0 +1,23 @@
+// fortune teller app 
+
+let fortune1 = "Your cat will look very cuddly today.";
+let fortune2 = "The weather will be nice tomorrow.";
+let fortune3 = "Be cautious of your new neighbor.";
+let fortune4 = "You will find a new hobby soon.";
+let fortune5 = "It would be wise to avoid the color red today.";
+
+// randomNumber = Math.random();
+
+// for (randomNumber (let i = 1, i <=5, i++){
+//     (if i: === 1){
+
+//     }
+    
+//     {
+//         randomNumber = for
+//     }
+// }
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
