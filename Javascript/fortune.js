dfd419801f35c88df5e73036f0b7a66b62da10d7
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-10-29 08:51:50 -0700

    finished the fortune app challenge in fcc

--- Javascript/fortune.js
+++ Javascript/fortune.js
@@ -5,19 +5,50 @@ let fortune2 = "The weather will be nice tomorrow.";
 let fortune3 = "Be cautious of your new neighbor.";
 let fortune4 = "You will find a new hobby soon.";
 let fortune5 = "It would be wise to avoid the color red today.";
+let selectedFortune;
 
-// randomNumber = Math.random();
+//switch: 
+// case: expression
+// break;
 
-// for (randomNumber (let i = 1, i <=5, i++){
-//     (if i: === 1){
+/*function genNumber(c){
+ return Math.floor(Math.random()*c);
+}
 
-//     }
-    
-//     {
-//         randomNumber = for
-//     }
-// }
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-    
-// }
+let randomNumber = genNumber(5);
+console.log(randomNumber);
+   */
+
+function genNumber(x){
+    return Math.floor(Math.random()*x);
+}
+
+let randomNumber = genNumber(5);
+
+
+switch (randomNumber){
+    case 1:
+        selectedFortune = fortune1;
+        console.log(selectedFortune);
+        break;
+    case 2:
+        selectedFortune = fortune2;
+        console.log(selectedFortune);
+        break;
+    case 3:
+        selectedFortune = fortune3;
+        console.log(selectedFortune);
+        break;
+    case 4:
+        selectedFortune = fortune4;
+        console.log(selectedFortune);
+        break;
+    case 5:
+        selectedFortune = fortune5;
+        console.log(selectedFortune);
+        break;
+    default:
+        selectedFortune = "Wrong day, try again";
+        console.log(selectedFortune);
+        break;
+}
