aa397c02e0cfeacfacc847627de120dd52532b1d
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-10-30 14:52:49 -0700

    started working on the calculator exercise/ lab fcc.

---
+++ Javascript/calculator.js
@@ -0,0 +1,13 @@
+function addTwoAndSeven (){
+return 2+7;
+
+}
+
+function addThreeAndFour(){
+    return 3+4;
+}
+
+addTwoAndSeven();
+
+console.log(addTwoAndSeven());
+console.log(addThreeAndFour());
