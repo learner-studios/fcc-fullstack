cb7c94e902623188bece6b5e3562454fe1a42d16
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-11-06 18:59:51 -0800

    keep practicing the email masker

--- Javascript/emailPractice2.js
+++ Javascript/emailPractice2.js
@@ -3,18 +3,20 @@ const emailString = "doej25346@gmail.com";
 
 function findKey(emailString) {
     let key = "@";
-    let keyAnswer =  emailString.indexOf(key) - 1; 
-    let stringSlice = emailString.slice(1,keyAnswer);
-    let stringReplace = emailString.
+    let keyAnswer =  emailString.indexOf(key) - 1; //I had before the (key) - 1
+    // let stringSlice = emailString.slice(1,keyAnswer);
+    let stringSubstring = emailString.substring(1,keyAnswer);
+    let stringReplace = emailString.replace(stringSubstring, "x");
 
-// return console.log(stringSlice);
-    return console.log(stringReplace);
+
+    // let stringReplace = emailString.replace(stringSlice,"x");
+    let stringRepeat = stringReplace.repeat(1, keyAnswer);
+
+// return console.log(keyAnswer);
+    // return console.log(stringSlice);
+    // return console.log(stringSubstring);
+    // return console.log(stringReplace);
+    // return console.log(stringRepeat);
 }
 
 findKey(emailString);
-
-// // simple slice function
-// function sliceString(){
-//     let stringSlice = emailString.slice(0,findKey);
-//     return console.log(stringSlice);
-// }
