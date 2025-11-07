97fe7dc1f479d50e6796c641a0b6e85e83b94b9f
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-11-07 14:44:16 -0800

    finished the loan check assignment lab

---
+++ Javascript/loanCheck.js
@@ -0,0 +1,33 @@
+// build the following variables
+let minIncomeForDuplex = 6000;
+let minCreditScoreForDuplex = 700;
+let minIncomeForCondo = 45000;
+let minCreditScoreForCondo = 680;
+let minIncomeForCar = 30000;
+let minCreditScoreForCar = 650;
+
+// create a message function 
+function getLoanMessage(annualIncome, creditScore){
+if (annualIncome >= minIncomeForDuplex && creditScore >= minCreditScoreForDuplex){
+    return "You qualify for a duplex, condo, and car loan.";
+}
+else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo){
+    return "You qulify for a condo and car loan.";
+}
+else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar){
+    return "You qualify for a car loan.";
+}
+else{
+    return "You don't qualify for any loans.";
+}
+}
+
+let duplexLoanMsg = getLoanMessage(85000, 850);
+let condoLoanMsg = getLoanMessage(65000, 690);
+let carLoanMsg = getLoanMessage(45000,660);
+let noLoanMsg  = getLoanMessage(25000, 550);
+
+console.log(duplexLoanMsg);
+console.log(condoLoanMsg);
+console.log(carLoanMsg);
+console.log(noLoanMsg);
