0ba410e54f0e43bea37a799128c592bc0fbcf6ed
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-11-10 14:41:17 -0800

    built the temperature converter

---
+++ Javascript/tempConvert.js
@@ -0,0 +1,10 @@
+//build a function to convert temperature from celcius to farenheit
+
+let tempNumb;
+
+function convertCtoF(tempNumb) {
+// let tempNumb;
+return console.log((tempNumb * (9/5)) + 32);
+}
+
+convertCtoF(30);
