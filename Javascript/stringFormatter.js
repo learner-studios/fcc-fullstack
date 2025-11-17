d219c64c00ebe54ed9e874079778a301c083de3d
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-11-17 07:57:35 -0800

    stating the string formatter workshop

---
+++ Javascript/stringFormatter.js
@@ -0,0 +1,6 @@
+let userInput = "   Hello World!   ";
+let cleanedInput = userInput.trim();
+console.log("Result of trimming whitespace from both ends:")
+console.log(cleanedInput);
+console.log("Original input:");
+console.log(userInput);
