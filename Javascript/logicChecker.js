f355f1d81190c0f7b2f8b36646ca8756bf2cace8
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-02 15:02:46 -0800

    practice with the logic checker

---
+++ Javascript/logicChecker.js
@@ -0,0 +1,9 @@
+// var truthyOrFalsy = "";
+
+// console.log(Boolean(truthyOrFalsy));
+
+var hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+    console.log("Timmy is employed as a developer.");
+}
