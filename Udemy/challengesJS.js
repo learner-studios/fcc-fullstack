31d74c1c3e33a26c14e96d49af7272b304e5615c
Jason <learner_studios@proton.me>
2025-12-07 14:26:09 -0800

    working on the Udemy course - JavaScript Basics.

---
+++ Udemy/challengesJS.js
@@ -0,0 +1,12 @@
+// store a variable to be used later in a prompt or console log alert
+
+let userName = " Jason";
+
+// count the amount of characters that are inside of the variable userName
+var nameTotalLength = userName.length;
+
+//output the content to the console
+console.log(nameTotalLength);
+
+console.log(`Hello ${userName}, welcome to the world of JavaScript!`);
+//mdn Math.min;
---
+++ Udemy/variable.js
@@ -0,0 +1,12 @@
+var a = '3';
+var b = '8';
+
+// so convert change the variables so that way the strings are changed:
+
+var c = a;
+a = b;
+b = c;
+
+// outputting the results
+console.log(`a is ${a}`);
+console.log(`b is ${b}`);
