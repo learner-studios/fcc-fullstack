9bd3feeb7e74cba27ccdc4aef4822862f8a80fbd
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-22 08:58:44 -0800

    practicing the truncate string exercise.

--- Javascript/truncateStringExercise.js
+++ Javascript/truncateStringExercise.js
@@ -1,5 +1,10 @@
 function truncateString(word, number) {
-    //use the slice
+
     //use the length property
+    let wordLengthTotal = word.length()
+    //use the slice
     //and write a conditional statement
+    console.log(wordLengthTotal);
 }
+
+truncateString("Lorem ipsum dolor sit amet", 10); // Output: "Lorem ipsu"
---
+++ Javascript/truncateStringPractice.js
@@ -0,0 +1,9 @@
+function truncateString(word, number){
+    var stringLength = word.length;
+
+    console.log(stringLength);
+
+    return stringLength;  
+}
+
+truncateString("Hello World", 5);  
