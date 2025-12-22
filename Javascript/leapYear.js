b083557e5668e8629fd723017ee6b94046e63552
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-22 14:52:20 -0800

    working on the leap year assignment from FCC.

--- Javascript/leapYear.js
+++ Javascript/leapYear.js
@@ -1,20 +1,11 @@
-// let result = '';
-let year = '2024';
+var year = 2024;
 
-function isLeapYear(year) {
-    if (year/4 && year != % 4) {
-        return console.log (`${year} is a leap year.`);
-    } else if (year/400 && % 400){
-        return console.log (`${year} is a leap year.`);
-    } else if (year %= 100) {
-        return console.log (`${year} is not a leap year.`);
+function isLeapYear(year){
+    if (year % 4 ===0){
+        return `${year} is a leap year`;
+    }   
+    else if (year % 100 ===0){
+        return `${year} is not a leap year`;
     }
-    else {
-        return console.log ("Please try your answer again");
-    }
+    else (year %)
 }
-// let result = year;
-
-console.log(isLeapYear());
-
-// need to use the modulo operator
--- Javascript/truncateStringPractice.js
+++
@@ -1,9 +0,0 @@
-function truncateString(word, number){
-    var stringLength = word.length;
-
-    console.log(stringLength);
-
-    return stringLength;  
-}
-
-truncateString("Hello World", 5);  
