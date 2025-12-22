d8895395ebedd627b719e6d91e2f7196d6edb23a
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-22 08:14:25 -0800

    Finished the mathbot assignment and updated files for the fcc fullstack project.

--- Javascript/logicChecker.js
+++ Javascript/logicChecker.js
@@ -1,9 +1,20 @@
 // var truthyOrFalsy = "";
 
 // console.log(Boolean(truthyOrFalsy));
-
+var isTimmyAGamer = false;
 var hasDeveloperJob = true;
+var timmyAge = 18;
 
 if (hasDeveloperJob) {
     console.log("Timmy is employed as a developer.");
+}
+if(isTimmyAGamer) {
+    console.log("Timmy loves to play World of Warcraft.");  
+}
+
+if (timmyAge >= 16) {
+    console.log("Timmy is old enough to drive.");
+}
+else {
+    console.log("Timmy is not old enough to drive.");
 }
