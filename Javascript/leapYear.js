dc890ea45327affe3426ccd6bc7fa83706af56b3
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-23 14:52:04 -0800

    finished some more assignments

--- Javascript/leapYear.js
+++ Javascript/leapYear.js
@@ -1,11 +1,16 @@
-var year = 2024;
-
+var year ;
 function isLeapYear(year){
     if (year % 4 ===0){
         return `${year} is a leap year`;
     }   
-    else if (year % 100 ===0){
+    else if (year % 100 != 0){
         return `${year} is not a leap year`;
     }
-    else (year %)
+    else if (year % 400 === 0){
+        return `${year} is a leap year`;
+ }
 }
+
+console.log(isLeapYear(2024));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(1900));
