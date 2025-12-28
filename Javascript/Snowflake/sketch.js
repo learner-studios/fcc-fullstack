c00b07d7b2e4f19816dbdf93cdd82eaf51dc85e5
Jason Palacio <meteor.man1989@gmail.com>
2025-12-27 17:48:13 -0800

    working and trying out the ps.j library for graphics in JS

---
+++ Javascript/Snowflake/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("lightblue");
+    strokeWeight(20);
+    fill("yellow");
+    stroke("orange");
+  circle(500,50,100);
+    
+    fill("red");
+    strokeWeight(22);
+    stroke("black");
+  triangle(30, 75, 58, 20, 86, 75);
+}
