583c13e95eeba4f2faf913c525905a7afcf9673d
Jason Palacio <meteor.man1989@gmail.com>
2025-12-29 05:54:15 -0800

    practicting with objects for today.

---
+++ Javascript/objectPractice.js
@@ -0,0 +1,11 @@
+const dog = {
+    name: 'Toby',
+    weight: '10 lbs',
+    age: 10,
+}
+
+showProperty1 = dog.name;
+//mdn typeof;
+typeofProperty1 = typeof(dog);
+console.log(typeofProperty1);
+console.log(showProperty1);
