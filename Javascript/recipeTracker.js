9f3cc6c26f0a42a5e82f59aecbecc656ec75a159
Jason Palacio <meteor.man1989@gmail.com>
2025-12-31 05:59:30 -0800

    working some more on the recipe tracker

--- Javascript/recipeTracker.js
+++ Javascript/recipeTracker.js
@@ -1,4 +1,4 @@
-let receipies = [];
+let recipes = [];
 
 const recipe1 = {
     name: "Spaghetti Carbonara",
@@ -11,7 +11,7 @@ const recipe1 = {
 const recipe2 = {
     name: "Chicken Curry",
     ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
-    cookingtime: 42, 
+    cookingTime: 42, 
     totalIngredients: null,
     difficultyLevel: "",
     
@@ -24,3 +24,38 @@ const recipe3 = {
     totalIngredients: null,
     difficultyLevel: "",
 }
+
+// need to practice accessing the objects
+// let recipe1Name = recipe1.name;
+// let recipe2CookingTime = recipe2.cookingTime;
+// let recipe3Ingredients = recipe3.ingredients;
+
+// console.log(recipe1Name);
+// console.log(recipe2CookingTime);
+// console.log(recipe3Ingredients);    
+
+//need to push all three objects into the array
+let recipesArray = recipes.push(recipe1, recipe2, recipe3);   
+console.log(recipes);
+
+//need to create a function with a single parameter takes an array with ingredients and returns the number of ingredients from the array passed to the function.
+
+function getTotalIngredients (ingredients){
+    let ingredientsArray = ingredients.length;
+    return ingredientsArray;
+}
+
+function getDifficultyLevel(cookingTime){
+    let cookingLevel = "";
+    if (cookingTime <= 30){
+        return cookingLevel = "easy";
+    }
+    else if (cookingTime <= 60){
+        return cookingLevel = "medium";
+    }
+    else {
+        return cookingLevel = "hard";
+    }
+}
+
+console.log
