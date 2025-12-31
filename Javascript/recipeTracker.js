b3132af210c56b3a607f0fd57523f8e2cd43dedf
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-30 19:21:08 -0800

    started working on the recipe tracker app

---
+++ Javascript/recipeTracker.js
@@ -0,0 +1,26 @@
+let receipies = [];
+
+const recipe1 = {
+    name: "Spaghetti Carbonara",
+    ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+    cookingTime: 22,
+    totalIngredients: null,
+    difficultyLevel: "",
+}
+
+const recipe2 = {
+    name: "Chicken Curry",
+    ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+    cookingtime: 42, 
+    totalIngredients: null,
+    difficultyLevel: "",
+    
+}
+
+const recipe3 = {
+    name: "Vegetable Stir Fry",
+    ingredients: ["broccoli", "carrot", "bell pepper"],
+    cookingTime: 15,
+    totalIngredients: null,
+    difficultyLevel: "",
+}
