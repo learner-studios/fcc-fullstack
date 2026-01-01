0a6cbfd356830744812c1446fbf74c99045ecbae
Jason Palacio <jpalacio@evergreenhealthcare.org>
2025-12-31 19:02:05 -0800

    need to fix the problems with the object

--- Javascript/quizGame.js
+++ Javascript/quizGame.js
@@ -1,11 +1,11 @@
 //create an array named questions.
 
-let questions = [
-    category: "Godzilla Movies",
-    questions: "How tall is Godzilla in the 2008 movie?",
-    choices: "A. 2 feet", "B. 355 feet", "C. 500 feet", "D. 1,000 feet",
-    answer: "C",
-];
-
-function getRandomQuestion() {
+// let questions = [
+//     category: "Godzilla Movies",
+//     questions: "How tall is Godzilla in the 2008 movie?",
+//     choices: "A. 2 feet", "B. 355 feet", "C. 500 feet", "D. 1,000 feet",
+//     answer: "C",
+// ];
+// 
+// function getRandomQuestion() {
 
