749a7405f125d1e53bb70bec3ce3e1f74c5c7ef6
Jason Palacio <meteor.man1989@gmail.com>
2026-01-06 19:31:29 -0800

    working on the next object assignment in JS for FCC.

---
+++ Javascript/recordCollection.js
@@ -0,0 +1,20 @@
+// create a 4 parameter function that keeps an for the four arguments and performs methods on them as objects records which is an object containing several individual albums.
+//id: a number represents a specific album in the records object.
+//prop:: a string representing the name of the album's property to update.
+//value - a string containing the information used to update the albums property 
+
+//input: update records criteria (always return the entire records object)
+//output: entire records object
+/** if value is an empty string then delete the given proper from the album
+ * 
+ * if prop isn't tracks and value isn't an empty string, assign the value to that albums prop
+ * 
+ * if prop is tracks and value isn't and empty string, but the album doesn't have a tracks property, create and empty array and add value to it.
+ * 
+ * if prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+ */
+
+
+function updateRecords(records,id,prop,value){
+    if (value === " ")
+}
---
+++ Javascript/recordsCollectionTest.js
@@ -0,0 +1,7 @@
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');    
+updateRecords(recordCollection, 5439, 'tracks', 'Take a Chance on Me');
+updateRecords(recordCollection, 2548, 'artist', '');
+updateRecords(recordCollection, 1245, 'tracks', 'Addicted to Love');
+updateRecords(recordCollection, 2468, 'tracks', 'Free');
+updateRecords(recordCollection, 2548, 'tracks', '');
+updateRecords(recordCollection, 1245, 'albumTitle', 'Riptide'); 
