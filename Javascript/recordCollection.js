3e97c081122b21fdd59596545499931a79d6790e
Jason Learner <jasonlearnerstudios@gmail.com>
2026-01-07 17:11:48 -0800

    finished the recordCollection assignment

--- Javascript/recordCollection.js
+++ Javascript/recordCollection.js
@@ -36,8 +36,41 @@ const recordCollection = {
 
 function updateRecords(records,id,prop,value){
     if (value === "") {
-       delete records[prop];
-       return updateRecords
+       delete records[id][prop];
+       return records
     }
-    else if 
+    else if (prop != "tracks" && value !=""){
+       records[id][prop] = value;
+      return records;
+    }
+    else if (prop === "tracks" && value != "" &&  !records[id][prop]){
+      records[id][prop] = [];
+      records[id][prop].push(value);
+      return records;
+    }
+    else if(prop === "tracks" && value !=""){
+      records[id][prop].push(value);
+      return records;
+    }
+    else {
+      return records;
+    }
 }
+
+// Import test cases
+// require('./recordsCollectionTest.js');
+// console.log(recordCollection);
+
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));;    
+console.log(updateRecords(recordCollection, 5439, 'tracks', 'Take a Chance on Me'));
+console.log(updateRecords(recordCollection, 2548, 'artist', ''));
+console.log(updateRecords(recordCollection, 1245, 'tracks', 'Addicted to Love'));
+console.log(updateRecords(recordCollection, 2468, 'tracks', 'Free'));
+console.log(updateRecords(recordCollection, 2548, 'tracks', ''));
+console.log(updateRecords(recordCollection, 1245, 'albumTitle', 'Riptide'));
+
+// Expected Output
+// { '1245': { artist: 'Robert Palmer', tracks: [ 'Addicted to Love' ], albumTitle: 'Riptide' },
+//   '2468': { albumTitle: '1999', artist: 'Prince', tracks: [ '1999', 'Little Red Corvette', 'Free' ] },
+//   '2548': { albumTitle: 'Slippery When Wet' , tracks: [ 'Let It Rock', 'You Give Love a Bad Name' ] },
+//   '5439': { albumTitle: 'ABBA Gold', artist: 'ABBA', tracks: [ 'Take a Chance on Me' ] } }
